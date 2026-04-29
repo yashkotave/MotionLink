5f37ddd5389f2f048999a4805727a504fab3d056
yashkotave <yashkotave1007@gmail.com>
2026-04-29 16:51:01 +0530

    Initialize Leaflet map with OpenStreetMap tile layer

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,10 @@ if(navigator.geoLocation){
     }
 );
 }
+
+const map = L.map('map').setView([0, 0], 10);
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: "OpenStreetMap"
+}).addTo(map);
