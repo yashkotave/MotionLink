e96571a23d6e9fb6afd6c278ee997599d149107a
yashkotave <yashkotave1007@gmail.com>
2026-04-29 17:37:49 +0530

    Handle live marker updates and user disconnection with Socket.IO

--- app.js
+++ app.js
@@ -10,12 +10,24 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function(socket){
+io.on("connection", function (socket) {
     console.log("connected");
-})
 
-app.get("/", function (req, res) {
-    res.render ("index");
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", {
+            id: socket.id,
+            latitude: data.latitude,
+            longitude: data.longitude
+        });
+    });
+
+    socket.on("disconnect", function () {
+        io.emit("user-disconnected", socket.id);
+    });
 });
 
-server.listen(3000);  
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+server.listen(3000);
--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,53 @@
 const socket = io();
 
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
 
-if(navigator.geoLocation){
-    navigator.geoLocation.watchPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        socket.emit("send-location", { latitude, longitude });
-    },(error) => {
-        console.log(error);
-    }, {
-        enableHighAccuracy : true,
-        timeout : 5000,
-        maximumAge : 0,
-    }
-);
+            console.log("My Location:", latitude, longitude);
+
+            socket.emit("send-location", {
+                latitude,
+                longitude
+            });
+        },
+        (error) => {
+            console.log("Geolocation Error:", error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        }
+    );
 }
 
-const map = L.map('map').setView([0, 0], 10);
+const map = L.map("map").setView([0, 0], 10);
 
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "OpenStreetMap"
 }).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    console.log("Received:", data);
+
+    const { id, latitude, longitude } = data;
+
+    map.setView([latitude, longitude], 16);
+
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
